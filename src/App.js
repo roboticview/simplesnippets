7186180d1f29e39b59711eb09ba03d43469f26ae
ROBOTICVIEW | Your Gateway to ML & AI <art@roboticview.com>
2024-05-08 22:21:33 -0400

    Simple React Snippets
    Simple React Snippets for Beginners

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import First from "./components/firstComponent";
 import "bootstrap/dist/css/bootstrap.css";
 import Greetings from "./components/greetingsComponent";
 import Payment from "./components/paymentComponent";
+// React Components
 function App() {
   return (
     <div className="App">
